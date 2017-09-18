7b8a7944211a2f424b834eb1527d0dd6f679a658
643699 <643699@ELF6IT215758.ACDM.DS.SAIT.CA>
2017-09-18 08:47:49 -0600

    New Project Creation Created Java servlet AgeCalculatorServlet Created new JSP ageCalculator

---
+++ src/java/sait/cprg352/AgeCalculatorServlet.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sait.cprg352;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 643699
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").
+                forward(request, response);
+        String Age = request.getParameter("currentage");
+        
+        if(Age == null || Age.isEmpty()){
+            request.setAttribute("errorMessage", "You must give your current age");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").
+                    forward(request, response);
+            
+        }
+        request.setAttribute("nextYearAge", Integer.parseInt(Age)+1);
+        
+            }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+}
